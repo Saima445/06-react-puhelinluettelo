9d0fa50fdf8477c3da9439d27480f1131f5c2cc9
SaimaS <saima.salonen@gmail.com>
2024-04-04 14:39:58 +0300

    pikku muutosta css:ään.

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,7 +2,7 @@ const Person = ({ person, deletePerson }) => {
   return (
     <li>
       <p className="p-width">{person.name}</p>
-      <p className="p-width">{person.number}</p>
+      <p className="p-width-number">{person.number}</p>
       <button className="delete-button" onClick={deletePerson}>
         delete
       </button>
